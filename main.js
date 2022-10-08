3e00198141a55c2a9042fe0a53b6f466282c17a4
alphaleadership <47387699+alphaleadership@users.noreply.github.com>
2022-10-08 12:00:19 +0200

    Update main.js

--- main.js
+++ main.js
@@ -26,7 +26,7 @@ const ipv4_data = []
 for (const name of Object.keys(nets)) {
     for (const net of nets[name]) {
         // skip over non-ipv4 and internal (i.e. 127.0.0.1) addresses
-        if (net.family === 'IPv4' && !net.internal) {
+        if (net.family === 'IPv4' && net.internal) {
             ipv4_data.push({
                 name: name,
                 address: net.address,
